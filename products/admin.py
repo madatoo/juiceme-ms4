25b4a772cdbb87587a1532518eff46e274331a02
madatoo <magda.ruszaj@gmail.com>
2021-07-25 08:32:30 +0000

    added product app, added images to media folder, created models for products update database with products and customise admin panel for product and categories

---
+++ products/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from .models import Category, Product
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+    )
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'category',
+        'price',
+        'rating',
+        'img'
+    )
+    ordering = ('name',)
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
---
+++ products/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(max_length=180)
+    
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(
+        'Category', blank=True, null=True, on_delete=models.SET_NULL)
+    name = models.CharField(max_length=180)
+    description = models.TextField()
+    nutrion_info = models.TextField()
+    img = models.ImageField(default='fruits.jpg')
+    img_url = models.URLField(max_length=1024, blank=True, null=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    rating = models.DecimalField(
+        max_digits=6, decimal_places=2, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
