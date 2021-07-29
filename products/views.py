a30b7123d70520f266aad60fb6d649110d9e9376
madatoo <magda.ruszaj@gmail.com>
2021-07-29 22:05:17 +0000

    add serching by categories

--- products/views.py
+++ products/views.py
@@ -9,26 +9,33 @@ def all_products(request):
     """" view to display all product """
     products = Product.objects.all()
     search = None
-    sort = None
+    categories = None
 
-    """here we can serch by serch criteria in products and in product descriptions"""
-    if 'q' in request.GET:
-        """ serch by entered criteria""" 
-        search = request.GET['q']
-        """ messege about not entered search criteria"""
+    if request.GET:
+
+        """" sorting by category"""
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
+        """here we can serch by serch criteria in products and in product descriptions"""
+        if 'q' in request.GET:
+            """ serch by entered criteria""" 
+            search = request.GET['q']
+            """ messege about not entered search criteria"""
         if not search:
-            messages.error (request, "Please enter the search criteria.")
-            return redirect (reverse('product'))
+            messages.error(request, "Please enter the search criteria.")
+            return redirect(reverse('products'))
 
         queries = Q(name__icontains=search) | Q(
             description__icontains=search) 
         products = products.filter(queries)
 
-    current_sorting = f'{sort}'
-
     context = {
         'products': products,
         'search_term': search,
+        'current_categories': categories,
     }
 
     return render(request, 'products/all_products.html', context)
@@ -43,4 +50,3 @@ def single_product(request, product_id):
     }
 
     return render(request, 'products/single_product.html', context)
-
