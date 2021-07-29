d1b144c4dcc5862e654bb1eac949bac20c5d5e7c
madatoo <magda.ruszaj@gmail.com>
2021-07-29 19:21:48 +0000

    added searching funnctionality

--- products/views.py
+++ products/views.py
@@ -1,13 +1,34 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Product
+from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.contrib import messages
+from django.db.models import Q
+from .models import Product, Category
 
 
 def all_products(request):
+
     """" view to display all product """
     products = Product.objects.all()
+    search = None
+    sort = None
+
+    """here we can serch by serch criteria in products and in product descriptions"""
+    if 'q' in request.GET:
+        """ serch by entered criteria""" 
+        search = request.GET['q']
+        """ messege about not entered search criteria"""
+        if not search:
+            messages.error (request, "Please enter the search criteria.")
+            return redirect (reverse('product'))
+
+        queries = Q(name__icontains=search) | Q(
+            description__icontains=search) 
+        products = products.filter(queries)
+
+    current_sorting = f'{sort}'
 
     context = {
         'products': products,
+        'search_term': search,
     }
 
     return render(request, 'products/all_products.html', context)
@@ -22,3 +43,4 @@ def single_product(request, product_id):
     }
 
     return render(request, 'products/single_product.html', context)
+
