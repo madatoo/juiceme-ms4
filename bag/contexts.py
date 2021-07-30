01804d7ea487d15adcfd4ae10594cf84c74a3c11
madatoo <magda.ruszaj@gmail.com>
2021-07-30 03:30:28 +0000

    updated shopping bag

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,16 +1,29 @@
-from django.conf import settings
+from django.shortcuts import get_object_or_404
 from products.models import Product
 
 
 def products_in_bag(request):
     """ the contents from here will be available for us in whole app"""
 
-    bag_products = []
+    bag_items = []
     total = 0
     product_count = 0
+    
+    bag = request.session.get('bag', {})
+
+    for product_id, quantity in bag.items():
+
+        product = get_object_or_404(Product, pk=id)
+        total += quantity*product.price
+        product_count += quantity
+        bag_items.append({
+            'product_id': product_id, 
+            'quantity': quantity,
+            'product': product,
+        })
 
     context = {
-        'bag_products': bag_products,
+        'bag_items': bag_items,
         'total': total,
         'product_count': product_count,
     }
