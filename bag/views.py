3b920544acfae280003f71d06f0cb3fc43fdaa9d
madatoo <magda.ruszaj@gmail.com>
2021-08-26 15:25:04 +0000

    added urls path for checkout app to settings.py in juiceme app

--- bag/views.py
+++ bag/views.py
@@ -9,13 +9,15 @@ def bag_page(request):
 
 def add_to_bag(request, product_id):
     """ add  a quantity product to the bag"""
-    #post = request.POST
+    # post = request.POST
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
-
+    
     if product_id in list(bag.keys()):
         bag[product_id] += quantity
+        print(quantity)
+    
     else:
         bag[product_id] = quantity
 
