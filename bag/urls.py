52ad0ce4631a9e21710e78cd557f110f108dc40d
madatoo <magda.ruszaj@gmail.com>
2021-08-31 07:49:17 +0000

    another try to solve the issue  with bag

--- bag/urls.py
+++ bag/urls.py
@@ -4,7 +4,7 @@ from .views import bag_page, add_to_bag, update_bag, delete_from_bag
 
 urlpatterns = [
     path('', bag_page, name='bag_page'),
-    path('add/<int:product_id>/', add_to_bag, name='add_to_bag'),
-    path('update/<int:product_id>/', update_bag, name='update_bag'),
-    path('delete/<int:product_id>/', delete_from_bag, name='delete_fom_bag'),
+    path('add/<item_id>/', add_to_bag, name='add_to_bag'),
+    path('update_bag/<item_id>/', update_bag, name='update_bag'),
+    path('delete/<item_id>/', delete_from_bag, name='delete_fom_bag'),
 ]
--- bag/views.py
+++ bag/views.py
@@ -4,19 +4,19 @@ from django.shortcuts import render, redirect, reverse, HttpResponse
 def bag_page(request):
     """A view to display the bag context page """
 
-    return render(request, 'bag/bag_page.html')
+    return render(request,'bag/bag_page.html')
 
 
-def add_to_bag(request, product_id):
+def add_to_bag(request, item_id):
     """ add a quantity for single product to the bag"""
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
 
-    if product_id in list(bag.keys()):
-        bag[product_id] += quantity
+    if item_id in list(bag.keys()):
+        bag[item_id] += quantity
     else:
-        bag[product_id] = quantity
+        bag[item_id] = quantity
 
     request.session['bag'] = bag
     return redirect(redirect_url)
@@ -28,21 +28,21 @@ def update_bag(request, product_id):
     bag = request.session.get('bag', {})
 
     if quantity > 0:
-        bag[product_id] = quantity
+        bag[id] = quantity
     else:
-        bag.pop(product_id)
+        bag.pop(id)
 
     request.session['bag'] = bag
     return redirect(reverse('bag_page'))
 
 
-def delete_from_bag(request, product_id):
+def delete_from_bag(request, item_id):
     """
     remove item from bag
     """
     try:
         bag = request.session.get('bag', {})
-        bag.pop(product_id)
+        bag.pop(item_id)
 
         request.session['bag'] = bag
         return HttpResponse(status=200)
