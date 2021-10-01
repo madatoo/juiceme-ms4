aab7b92baf8681d5120ce8566913f818ec9006b3
madatoo <magda.ruszaj@gmail.com>
2021-10-01 19:11:44 +0000

    installed stripe, add stripe_currency field in settings.py

--- checkout/models.py
+++ checkout/models.py
@@ -22,7 +22,7 @@ class OrderLineItem(models.Model):
     order = models.ForeignKey(Order, null=False, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, null=False, on_delete=models.CASCADE)
     quantity = models.IntegerField(blank=False)
-   
+
     def __str__(self):
         return "{0} {1} @ {2}".format(
             self.quantity, self.product.name, self.product.price)
--- juiceme/settings.py
+++ juiceme/settings.py
@@ -211,7 +211,7 @@ if 'USE_AWS' in os.environ:
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Stripe
-
+STRIPE_CURRENCY = 'eur'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
