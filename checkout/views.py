55d9a67118f4abd5a4e255689d445403f0766cb4
madatoo <magda.ruszaj@gmail.com>
2021-10-04 14:42:05 +0000

    removed errors which allows me display the checkout page

--- checkout/views.py
+++ checkout/views.py
@@ -1,16 +1,19 @@
 from django.shortcuts import render, redirect, reverse
+from django.conf import settings
 from django.contrib import messages
 
 from .forms import OrderForm
 from .models import Order, OrderLineItem
 
 import os
+import stripe
+
+stripe.api_key = settings.STRIPE_SECRET_KEY
 
 
 def checkout(request):
     """"source CI video"""
     stripe_public_key = os.getenv('STRIPE_PUBLIC_KEY', '')
-    stripe_secret_key = os.getenv('STRIPE_SECRET_KEY', '')
 
     bag = request.session.get('bag', {})
     if not bag:
@@ -22,7 +25,6 @@ def checkout(request):
     context = {
         'order_form': order_form,
         'stripe_public_key': stripe_public_key,
-        'client_secret': intent.client_secret
-    }
+        }
 
     return render(request, template, context)
