5e5ac09a3f503527414b835acda202ecfd8a113e
madatoo <magda.ruszaj@gmail.com>
2022-01-07 23:59:45 +0000

    added home css folder and restyled home page, delete unused code from base.css and added file index.css for home page

--- home/views.py
+++ home/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
 
-def index (request):
+def index(request):
     """A view to return the home page (index.html) """
 
-    return render (request, "home/index.html")
+    return render(request, "home/index.html")
