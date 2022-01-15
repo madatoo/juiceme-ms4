e55f792a984b70a1bb3a5040d802a3b62c8113f8
madatoo <magda.ruszaj@gmail.com>
2022-01-15 21:18:08 +0000

    added blank lines required and fix spelling error in signals.py

--- checkout/signals.py
+++ checkout/signals.py
@@ -1,19 +1,20 @@
 from django.db.models.signals import post_save, post_delete
-from django.dispatch import reciver
+from django.dispatch import receiver
 
 from .models import OrderLineItem
 
-@reciver(post_save, sender=OrderLineItem)
+
+@receiver(post_save, sender=OrderLineItem)
 def update_on_save(sender, instance, created, **kwargs):
     """
     update order total on lineitem update/create
     """
     instance.order.update_total()
 
-@reciver(post_delete, sender=OrderLineItem)
+
+@receiver(post_delete, sender=OrderLineItem)
 def update_on_delete(sender, instance, created, **kwargs):
     """
     update order total on lineitem delete
     """
     instance.order.update_total()
-    
