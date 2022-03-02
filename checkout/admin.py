0210395ed5b08d87c789f54ee9bd3e12d135bf57
madatoo <magda.ruszaj@gmail.com>
2022-03-02 16:25:35 +0000

    apply the migrations for checkout and update the admin.py

--- checkout/admin.py
+++ checkout/admin.py
@@ -10,8 +10,19 @@ class OrderLineItemAdminInline(admin.TabularInline):
 class OrderAdmin(admin.ModelAdmin):
     inlines = (OrderLineItemAdminInline,)
 
-    list_display =('order_number', 'date', 'full_name', 'delivery_cost', 'discount', 'order_total', 'total')
+    fields = ('order_number', 'date', 'full_name',
+              'email', 'phone_number', 'country',
+              'postcode', 'town_or_city', 'street_address1',
+              'street_address2', 'county', 'delivery_cost',
+              'order_total', 'total', 'original_bag',
+              'stripe_pid')
+
+    list_display = ('order_number', 'date',
+                    'full_name', 'delivery_cost',
+                    'discount', 'order_total',
+                    'total', 'orginal_bag', 'stripe_pid')
 
     ordering = ('-date',)
+
 
 admin.site.register(Order, OrderAdmin)
