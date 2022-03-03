1cc9ca08f3f978653ae877a488816818c216cdc1
madatoo <magda.ruszaj@gmail.com>
2022-03-03 01:38:58 +0000

    added webhook handler

---
+++ checkout/webhook_handler.py
@@ -0,0 +1,17 @@
+"""source CI """
+from django.http import HttpResponse
+
+
+class StripeWH_Handler:
+    """Handle Stripe webhooks"""
+
+    def __init__(self, request):
+        self.request = request
+
+    def handle_event(self, event):
+        """
+        Handle a generic/unknown/unexpected wh event
+        """
+        return HttpResponse(
+            content=f'Webhook recived: {event["type"]}',
+            status=200)
