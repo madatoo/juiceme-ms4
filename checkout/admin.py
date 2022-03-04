2aab0d59abe8001764a312676899ea74947c94ea
madatoo <magda.ruszaj@gmail.com>
2022-03-04 00:18:36 +0000

    removed whitespaces and fixed the indentations error from checkout/admin.py

--- checkout/admin.py
+++ checkout/admin.py
@@ -6,15 +6,16 @@ from .models import Order, OrderLineItem
 class OrderLineItemAdminInline(admin.TabularInline):
     """source CI"""
     model = OrderLineItem
-    fields = ('lineitem_total', )
+    fields = ('lineitem_total',)
 
 
 class OrderAdmin(admin.ModelAdmin):
+    """order admin"""
     inlines = (OrderLineItemAdminInline,)
 
     readonly_fields = (
-        'order_number', 'date', 'delivery_cost',
-        'order_total', 'total', 'orginal_bag', 'stripe_pid')
+        'order_number', 'date', 'delivery_cost', 'order_total',
+        'total', 'orginal_bag', 'stripe_pid')
 
     fields = ('order_number', 'date', 'full_name',
               'email', 'phone_number', 'country',
