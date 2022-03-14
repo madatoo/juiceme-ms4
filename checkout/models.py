5bbd5a9c6ec3a14b87b5b7cd9561ac8a31012b8c
madatoo <magda.ruszaj@gmail.com>
2022-03-14 18:01:19 +0000

    updated order model

--- checkout/models.py
+++ checkout/models.py
@@ -35,14 +35,14 @@ class Order(models.Model):
     email = models.EmailField(max_length=254, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
     country = models.CharField(
-        max_length=20, null=False, blank=False, default="Ireland")
+        max_length=20, null=False, blank=False, default="IE")
     postcode = models.CharField(max_length=20, null=True, blank=True)
     town_or_city = models.CharField(max_length=40, null=False, blank=False)
     street_address1 = models.CharField(max_length=80, null=False, blank=False)
     street_address2 = models.CharField(max_length=80, null=True, blank=True)
     county = models.CharField(
         max_length=20, null=True, blank=True, choices=COUNTY_CHOICES)
-    date = models.DateTimeField(auto_now_add=True)
+    date = models.DateTimeField()
     delivery_cost = models.DecimalField(
         max_digits=6, decimal_places=2, default=0)
     discount = models.DecimalField(
