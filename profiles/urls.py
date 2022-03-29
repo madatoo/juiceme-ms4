3426af8e55a3f94c1b8650c95e7655c04c60cd49
madatoo <magda.ruszaj@gmail.com>
2022-03-29 14:42:42 +0000

    added order history and updated checkout views

--- profiles/urls.py
+++ profiles/urls.py
@@ -5,4 +5,6 @@ from . import views
 
 urlpatterns = [
     path('', views.display_profile, name='display_profile'),
+    path('order_history/<order_number>',
+         views.order_history, name='order_history'),
 ]
