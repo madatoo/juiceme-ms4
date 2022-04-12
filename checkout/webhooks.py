88af503e94dbf8e781fadeeed99c2ea89ccedee5
madatoo <magda.ruszaj@gmail.com>
2022-04-12 18:29:49 +0000

    removed print statement and add doc string at the top of file

--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -1,3 +1,6 @@
+"""
+webhooks handlers for payment_intent.succeeded and payment_failed
+"""
 from django.conf import settings
 from django.http import HttpResponse
 from django.views.decorators.http import require_POST
@@ -34,16 +37,14 @@ def webhook(request):
     except Exception as e:
         return HttpResponse(content=e, status=400)
 
-    print('Success')
-    return HttpResponse(status=200)
-
     # Set up a webhook handler
     handler = StripeWH_Handler(request)
 
     # Map webhook events to relevant handler functions
     event_map = {
         'payment_intent.succeeded': handler.handle_payment_intent_succeeded,
-        'payment_intent.payment_failed': handler.handle_payment_intent_payment_failed,
+        'payment_intent.payment_failed':
+        handler.handle_payment_intent_payment_failed,
     }
 
     # Get the webhook type from Stripe
