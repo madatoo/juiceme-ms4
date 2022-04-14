28873b86ced508044f1f8cbf6966875d20a32350
madatoo <magda.ruszaj@gmail.com>
2022-04-14 14:26:23 +0000

    added the product folder for custom_widget_templates and tested the edit product page

--- products/forms.py
+++ products/forms.py
@@ -15,3 +15,12 @@ class ProductForm(forms.ModelForm):
         label='Image', required=False,
         widget=CustomClearableFileInput
     )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # categories = Category.objects.all()
+        # names = [(c.id, c.name()) for c in categories]
+
+        # self.fields['category'].choices = names
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-black rounded-0'
