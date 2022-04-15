350b475c8b6d2241ea444af0ae6dbf5941a5e231
madatoo <magda.ruszaj@gmail.com>
2022-04-15 06:26:39 +0000

    added single_question file and url

--- faq/views.py
+++ faq/views.py
@@ -1,15 +1,23 @@
 """views for FAQ app"""
-from django.shortcuts import render
+from django.shortcuts import (render, redirect,
+                              reverse, get_object_or_404)
 from .models import FaqPosts
 
 
 def faq_all_questions(request):
     """ this is the view for all questions"""
 
-    all_questions = FaqPosts.objects.all().order_by('-date_added')
+    faqpost = FaqPosts.objects.all().order_by('-date_added')
 
     context = {
-        'all_questions': all_questions
+        'faqpost': faqpost
     }
 
     return render(request, 'faq/faq_guestions.html')
+
+
+def single_question(request, faq_id):
+    """ view to display single faq question"""
+    faq = get_object_or_404(FaqPosts, pk=faq_id)
+
+    return redirect(reverse('faq/single_guestions.html', args=[faq_id]))
