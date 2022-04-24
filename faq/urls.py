41ea4bbb9d67c09401a46fc233943d5c92659fbb
madatoo <magda.ruszaj@gmail.com>
2022-04-24 15:45:36 +0000

    added edit and delete FAQ post functionality and tested how it works - test passed

--- faq/urls.py
+++ faq/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('', views.faq_all_questions, name='faq_all_questions'),
     path('<int:faq_id>/', views.single_question, name='single_question'),
     path('add/', views.add_question, name='add_question'),
+    path('edit/<int:faq_id>/', views.edit_question, name='edit_question'),
+    path('delete/<int:faq_id>/', views.delete_question,
+         name='delete_question'),
 ]
--- faq/views.py
+++ faq/views.py
@@ -29,6 +29,7 @@ def single_question(request, faq_id):
 
     return render(request, 'faq/single_question.html', context)
 
+
 @login_required()
 def add_question(request):
     """ Add a question to the FAQ list"""
@@ -57,3 +58,42 @@ def add_question(request):
     }
 
     return render(request, template, context)
+
+
+@login_required()
+def edit_question(request, faq_id):
+    """ view for editing FAQ post by the admin"""
+    faq = get_object_or_404(FaqPosts, pk=faq_id)
+    if request.method == 'POST':
+        form = FaqPostsForm(request.POST, request.FILES, instance=faq)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "FAQ Post successfuly edited.")
+            return redirect(reverse('single_question', args=[faq.id]))
+        else:
+            messages.error(request, "Something went wrong. \
+                Please enter the details again.")
+    else:
+        form = FaqPostsForm(instance=faq)
+        messages.info(request, f'You are editing {faq.question}')
+
+    template = 'faq/edit_question.html'
+    context = {
+        'form': form,
+        'faq': faq
+    }
+
+    return render(request, template, context)
+
+
+@login_required()
+def delete_question(request, faq_id):
+    """ Delete product from store """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+
+    faq = get_object_or_404(FaqPosts, pk=faq_id)
+    faq.delete()
+    messages.success(request, 'FAQ post deleted!')
+    return redirect(reverse('faq_all_questions'))
