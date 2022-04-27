ab06ca91c8d7bee26f55541170cab04d1c20b25f
madatoo <magda.ruszaj@gmail.com>
2022-04-27 17:00:12 +0000

    fixed update details form - working as expected

--- profiles/forms.py
+++ profiles/forms.py
@@ -3,35 +3,52 @@ from django import forms
 from .models import UserProfile
 
 
+COUNTY_CHOICES = (
+        ('Antrim', 'Antrim'), ('Armagh', 'Armagh'), ('Carlow', 'Carlow'),
+        ('Cavan', 'Cavan'), ('Clare', 'Clare'), ('Cork', 'Cork'),
+        ('Derry', 'Derry'), ('Donegal', 'Donegal'), ('Down', 'Down'),
+        ('Dublin', 'Dublin'), ('Fermanagh', 'Fermanagh'), ('Galway', 'Galway'),
+        ('Kerry', 'Kerry'), ('Kildare', 'Kildare'), ('Kilkenny', 'Kilkenny'),
+        ('Laois', 'Laois'), ('Letrim', 'Letrim'), ('Limerick', 'Limerick'),
+        ('Longford', 'Longford'), ('Louth', 'Louth'), ('Mayo', 'Mayo'),
+        ('Meath', 'Meath'), ('Monaghan', 'Monaghan'), ('Offlay', 'Offlay'),
+        ('Roscommon', 'Roscommon'), ('Sligo', 'Sligo'),
+        ('Tipperary', 'Tipperary'), ('Tyrone', 'Tyrone'),
+        ('Wateford', 'Waterford'), ('Westmeath', 'Westmeath'),
+        ('Wexford', 'Wexford'), ('Wicklow', 'Wicklow'),
+    )
+
+
 class UserProfileForm(forms.ModelForm):
     """form for updating the default address"""
-    
+
     class Meta:
         """ all data can be updated except\
         the user and country fields """
         model = UserProfile
+
+        fields = ['default_phone_number', 'default_street_address1',
+                  'default_street_address2', 'default_town_or_city',
+                  'default_postcode', 'default_county']
+
+        default_county = forms.ChoiceField(choices=COUNTY_CHOICES)
 
         exclude = ('user',)
 
     def __init__(self, *args, **kwargs):
         """"add placeholders to the form"""
         super().__init__(*args, **kwargs)
-        placeholders = {
-            'default_phone_number': 'Phone',
-            'default_street_address1': 'Address 1',
-            'default_street_address2': 'Address 2',
-            'default_town_or_city': 'Town or City',
-            'default_postcode': 'Postal Code',
-            'default_county':'County'
-        }
-
         self.fields['default_phone_number'].widget.attrs['autofocus'] = True
+        self.fields['default_county'].choices = COUNTY_CHOICES
+        self.fields['default_phone_number'].widget.attrs[
+            'placeholder'] = 'Phone'
+        self.fields['default_street_address1'].widget.attrs[
+            'placeholder'] = 'Address 1'
+        self.fields['default_street_address2'].widget.attrs[
+            'placeholder'] = 'Address 2'
+        self.fields['default_town_or_city'].widget.attrs[
+            'placeholder'] = 'Town or City'
+        self.fields['default_postcode'].widget.attrs[
+            'placeholder'] = 'Postal Code'
         for field in self.fields:
-            if field != 'default_country':
-                if self.fields[field].required:
-                    placeholder = f'{placeholders[field]} *'
-                else:
-                    placeholder = placeholders[field]
-            self.fields[field].widget.attrs['placeholder'] = placeholder
-            self.fields[field].widget.attrs['class'] = 'stripe-style-input'
             self.fields[field].label = False
